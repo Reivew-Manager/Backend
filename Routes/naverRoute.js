f574cc46059bae3839d1b59f0d4840a934fdbe65
eojin0814 <leeeojin6@gmail.com>
2023-11-12 13:35:39 +0900

    feat : MVC

--- Routes/naverRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const naver = require('../src/app/Naver/naverController.js');
-
-// Define your routes using the router object
-router.get('/reviewGather', naver.getAllReviews);
-
-module.exports = router;
